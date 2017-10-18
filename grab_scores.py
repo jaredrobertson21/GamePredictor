eb86bb4121a27d952c882af8496411174037cbae
Jared Robertson <jared.robertson@usask.ca>
2017-10-17 19:33:12 -0600

    Ability to grab winners, losers, and scores complete

--- grab_scores.py
+++ grab_scores.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://baseball-reference.com'
+url = 'https://hockey-reference.com'
 req = requests.get(url)
 html = BeautifulSoup(req.text, 'html.parser')
 
 games_summary = html.find_all(class_='game_summary')
 
+daily_games_results = []
+
 for game in games_summary:
-	if game.a:
-		print game.text.strip()
+    game_result = dict()
+    game_result['winner'] = game.find(class_='winner').find('a').get_text()
+    game_result['winner_score'] = game.find(class_='winner').find(class_='right').get_text()
+    game_result['loser'] = game.find(class_='loser').find('a').get_text()
+    game_result['loser_score'] = game.find(class_='loser').find(class_='right').get_text()
+    daily_games_results.append(game_result)
